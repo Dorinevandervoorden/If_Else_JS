b2296d751a13c65aae4dd7f5df3bc50a4087a9d3
Dorine van der Voorden <84196122+Dorinevandervoorden@users.noreply.github.com>
2021-06-11 08:03:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log("Hallo Winc Academy Studenten");
+
+const age = 26;
+
+//Deze gaat over de leeftijd
+if (age > 18) {
+
+    document.write("Welkom bij de Winc Bar");
+
+}
+else {
+
+    document.write("Sorry, je bent te jong!..")
+
+}
+
+console.log('age', age);
+
+//Deze gaat over het geslacht
+const isFemale = true;
+
+if (isFemale) {
+
+    document.write("Hallo dames!!");
+
+}
+else {
+
+    document.write("Sorry heren, ladies only..!");
+
+}
+
+console.log('isFemale', isFemale);
+
+//Deze gaat over veilig thuiskomen
+const driverStatus = 'bob';
+
+if (driverStatus == 'bob') {
+
+    document.write("Jij mag rijden");
+}
+else {
+
+    document.write("Nee sorry, we regelen een taxi voor je");
+
+}
+
+console.log('driverStatus', driverStatus);
